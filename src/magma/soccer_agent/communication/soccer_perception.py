270d55fbc3fac25fb0f778dd09fa3c8c70be091b
Stefan Glaser <stefan.glaser@hs-offenburg.de>
2025-04-13 11:19:18 +0200

    Added soccer specific perceptors.
    * Only a generic soccer game state perceptor for now.

---
+++ src/magma/soccer_agent/communication/soccer_perception.py
@@ -0,0 +1,40 @@
+from dataclasses import dataclass
+
+from magma.agent.communication.perception import Perceptor
+
+
+@dataclass(frozen=True)
+class SoccerGameStatePerceptor(Perceptor):
+    """
+    Perceptor representing a soccer game state.
+    """
+
+    play_time: float
+    """
+    The current play time.
+    """
+
+    play_side: str
+    """
+    The current play side.
+    """
+
+    play_mode: str
+    """
+    The current play mode.
+    """
+
+    player_no: int
+    """
+    The player number.
+    """
+
+    score_left: int
+    """
+    The left team score.
+    """
+
+    score_right: int
+    """
+    The right team score.
+    """
