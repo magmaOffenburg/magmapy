1c79c09357d4747b771849f6e51b7ac077096093
Stefan Glaser <stefan.glaser@hs-offenburg.de>
2025-04-13 11:15:49 +0200

    Added agent runtime components.
    * Added agent runtime for running the main agent loop.
    * Added agent factory for creating individual components required by the agent runtime.

---
+++ src/magma/agent/runtime/factory.py
@@ -0,0 +1,17 @@
+from typing import Protocol
+
+from magma.agent.communication.channel_manager import PChannelManager
+from magma.agent.decision.behavior import PBehavior
+from magma.agent.decision.decision_maker import PDecisionMaker
+from magma.agent.model.agent_model import PMutableAgentModel
+
+
+class PAgentFactory(Protocol):
+    """
+    Protocol for an agent component factory.
+    """
+
+    def create_agent_components(self) -> tuple[PChannelManager, PMutableAgentModel, dict[str, PBehavior], PDecisionMaker]:
+        """
+        Create a set of core agent components.
+        """
