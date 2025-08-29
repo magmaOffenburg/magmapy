f806deed67e7af4931fa7d0ed80c28567fd9f7db
Stefan Glaser <stefan.glaser@hs-offenburg.de>
2025-08-29 13:09:42 +0200

    Minor refactoring of soccer rules.
    * Use final instead of private fields and removed corresponding getter methods.

--- src/magma/soccer_agent/model/soccer_rules.py
+++ src/magma/soccer_agent/model/soccer_rules.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from typing import Final
+
 
 class SoccerRules:
     """Class for representing soccer specific rules."""
@@ -13,27 +15,14 @@ class SoccerRules:
     ) -> None:
         """Create a new rule book."""
 
-        self._duration: float = match_duration
-        self._overtime: float = match_overtime
-        self._kick_off_time: float = kick_off_time
-        self._throw_in_time: float = throw_in_time
+        self.duration: Final[float] = match_duration
+        """The total duration of a normal soccer game."""
 
-    def get_duration(self) -> float:
-        """Retrieve the total duration of a normal soccer game."""
+        self.overtime: Final[float] = match_overtime
+        """The total overtime of an extended soccer game."""
 
-        return self._duration
+        self.kick_off_time: Final[float] = kick_off_time
+        """The time a team has to perform a kick-off before the game continues normally and the other team is allowed to approach the ball again."""
 
-    def get_overtime(self) -> float:
-        """Retrieve the total overtime of a extended soccer game."""
-
-        return self._overtime
-
-    def get_kick_off_time(self) -> float:
-        """Retrieve the time a team has to perform a kick-off before the game continues normally and the other team is allowed to approach the ball again."""
-
-        return self._kick_off_time
-
-    def get_throw_in_time(self) -> float:
-        """Retrieve the time a team has to perform a throw-in before the game continues normally and the other team is allowed to approach the ball again."""
-
-        return self._throw_in_time
+        self.throw_in_time: Final[float] = throw_in_time
+        """The time a team has to perform a throw-in before the game continues normally and the other team is allowed to approach the ball again."""
